9765b3ddf6749aafd55008a2f1dad69e58ccaa41
PatrickCabigan <jgcabigan@student.apc.edu.ph>
2025-12-30 23:22:20 +0800

    updated new personalized web

--- Introduction/js/javascript.js
+++ Introduction/js/javascript.js
@@ -1,23 +1,25 @@
-// Dynamic Greeting based on time
+// Dynamic Greeting Logic
 document.addEventListener("DOMContentLoaded", function() {
     const greeting = document.getElementById("greeting");
     const hour = new Date().getHours();
-    if (hour < 12) greeting.innerText = "Good Morning, I'm Patrick!";
-    else if (hour < 18) greeting.innerText = "Good Afternoon, I'm Patrick!";
-    else greeting.innerText = "Good Evening, I'm Patrick!";
+    
+    if (hour < 12) greeting.innerHTML = "Good Morning!";
+    else if (hour < 18) greeting.innerHTML = "Good Afternoon!";
+    else greeting.innerHTML = "Good Evening!";
 });
 
-// Back to Top Button Logic
+// Back to Top Button Visibility
 let mybutton = document.getElementById("btn-back-to-top");
 
 window.onscroll = function() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
         mybutton.style.display = "block";
     } else {
         mybutton.style.display = "none";
     }
 };
 
+// Scroll to Top action
 mybutton.onclick = function() {
     window.scrollTo({top: 0, behavior: 'smooth'});
 };
