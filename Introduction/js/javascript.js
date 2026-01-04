b63aa99ab3742ee4ce4e3ba4a7a46ae9e5bd378c
PatrickCabigan <jgcabigan@student.apc.edu.ph>
2026-01-04 16:13:05 +0800

    testing

--- Introduction/js/javascript.js
+++ Introduction/js/javascript.js
@@ -1,16 +1,7 @@
-
 function toggleDarkMode() {
-    // We target the 'body' element
     var element = document.body;
-    
-    // We toggle the class 'dark-mode' defined in our CSS
-    element.classList.toggle("dark-mode");
-    
-    // Logic check in console (F12 to see)
-    console.log("Aesthetic mode has been toggled!");
+    element.classList.toggle("dark-theme");
+    console.log("Aesthetic Switch toggled!");
 }
 
-// Optional: Console log to verify the file is connected upon loading
-window.onload = function() {
-    console.log("javascript.js is successfully connected to index.html");
-};
+console.log("javascript.js is connected and ready!");
