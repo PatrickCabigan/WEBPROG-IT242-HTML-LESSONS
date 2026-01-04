e56ab5507ae4001043c2da111b65909798872c8d
PatrickCabigan <jgcabigan@student.apc.edu.ph>
2026-01-04 22:10:41 +0800

    fixed errors

--- Introduction/js/javascript.js
+++ Introduction/js/javascript.js
@@ -1,8 +1,3 @@
-// Toggle logic for John Patrick
 function toggleTheme() {
     document.body.classList.toggle("dark-theme");
-    console.log("Aesthetic mode changed!");
 }
-
-// Log check
-console.log("javascript.js linked and active.");
